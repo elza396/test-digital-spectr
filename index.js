5c33d275217e0ca1a52e3aa782334aebe4198a60
elza396 <elza396@yandex.ru>
2021-03-20 15:03:23 +0500

    + вариант c fetch

--- index.js
+++ index.js
@@ -2,27 +2,27 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector(".form");
-    form.addEventListener("submit", e => {
+    form.addEventListener("submit", async e => {
         e.preventDefault();
 
         const formData = new FormData(form);
 
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://60376bfd5435040017722533.mockapi.io/form");
-        xhr.send(formData);
+        try {
+            const response = await fetch("http://60376bfd5435040017722533.mockapi.io/form", {
+                method: "POST",
+                body: formData,
+            })
 
-        const onError = () => {
-            alert("Ошибочка");
-        }
-
-        xhr.onload = () => {
-            if (xhr.status !== 201) {
-                onError();
-            } else {
-                alert(JSON.parse(xhr.response).text);
+            if (response.status !== 201) {
+                throw new Error(`Ошибка: ${response.status}`);
             }
+
+            const obj = await response.json();
+            alert(obj.text);
+        } catch (e) {
+            console.error(e);
+            alert("Ошибочка!");
         }
-        xhr.onerror = onError;
     })
 
 })
